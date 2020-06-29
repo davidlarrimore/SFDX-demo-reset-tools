57a2173b45cc746f5b22e0b60fa817bafcae99dd
David Larrimore <davidlarrimore@gmail.com>
2020-06-28 22:57:35 -0400

    Data Creation

--- scripts/CreateOrg.sh
+++ scripts/CreateOrg.sh
@@ -6,7 +6,8 @@ echo "*** Pushing metadata to scratch org ..."
 sfdx force:source:push
 echo "*** Assigning permission set to your user ..."
 sfdx force:user:permset:assign --permsetname Demo_Reset_Tools
+sfdx force:user:permset:assign --permsetname Demo_Reset_Tools_Testing_Permission_Set
 echo "*** Generating password for your user ..."
 sfdx force:user:password:generate --targetusername MyComponentsScratch
-#echo "*** Creating data"
-#sfdx ETCopyData:import -c data/ETCopyData --loglev
+echo "*** Creating data"
+sfdx force:data:tree:import -f ./data/data-Account.json,./data/data-Contact.json,./data/data-Demo_Reset_Task__c.json -u MyComponentsScratch
--- scripts/DeleteAllScratchOrgs.sh
+++ scripts/DeleteAllScratchOrgs.sh
@@ -1,6 +1,6 @@
 echo "Exporting scratch orgs..."
-sfdx force:data:soql:query -q "SELECT Id, Username FROM ScratchOrgInfo" -r csv -u MyComponents  > ./ScratchOrgs.csv
+sfdx force:data:soql:query -q "SELECT Id, Username FROM ScratchOrgInfo" -r csv -u gpbu-devhub > ./ScratchOrgs.csv
 cat ./ScratchOrgs.csv
 echo "Deleting found scratch Orgs..."
-sfdx force:data:bulk:delete -f ./ScratchOrgs.csv -s ScratchOrgInfo -w 10 -u MyComponents
+sfdx force:data:bulk:delete -f ./ScratchOrgs.csv -s ScratchOrgInfo -w 10 -u gpbu-devhub
 rm -f ./ScratchOrgs.csv
