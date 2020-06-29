c2c0eb3a5adb07eb65aa197390808d6616e632e8
David Larrimore <davidlarrimore@gmail.com>
2020-06-29 13:13:15 -0400

    Updated Project Deployment Script

--- scripts/DeployProject.sh
+++ scripts/DeployProject.sh
@@ -1,4 +1,4 @@
 echo "*** Deploying Source to Org ..."
 sfdx force:source:deploy --targetusername MyComponents --sourcepath force-app
 echo "*** Creating Package ..."
-sfdx force:package:create --name "Demo Reset Tools" --packagetype Unmanaged --path "force-app" --targetdevhubusername MyComponents
+sfdx force:package:create --name "Demo Reset Tools" --packagetype Unlocked --path "force-app" --targetdevhubusername MyComponents
