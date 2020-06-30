0ee8bcd964bcc5ef4eb2224e5f0eae8afff1f84d
David Larrimore <davidlarrimore@gmail.com>
2020-06-30 17:20:01 -0400

    Version 1.0!

--- README.md
+++ README.md
@@ -25,13 +25,16 @@ If you are familiar with [Apex](https://developer.salesforce.com/docs/atlas.en-u
 
 I am a pre-sales Solutions Engineer for [Salesforce](https://www.salesforce.com) and I develop solutions for my customers to demonstrate the capabilities of the amazing Salesforce platform. *This package represents functionality that I have used for demonstration purposes  and the content herein is definitely not ready for actual production use; specifically, it has not been tested extensively nor has it been written with security and access controls in mind. By installing this package, you assume all risk for any consequences and agree not to hold me or my company liable.*  If you are OK with that ...
 
-Simply click the button below and log into your org:
+
+If you just want the managed package, Version 1 can be installed here (Password: test1234): https://login.salesforce.com/packaging/installPackage.apexp?p0=04t3h000003wQg0AAE
+
+
+If you want to modify the code to your own liking, use this link to copy over the metadata to your org:
 
 <a href="https://githubsfdeploy.herokuapp.com">
   <img alt="Deploy to Salesforce"
        src="https://raw.githubusercontent.com/afawcett/githubsfdeploy/master/src/main/webapp/resources/img/deploy.png">
 </a>
-
 
 Or click here to deploy to a DevHub Org:
 
--- scripts/DeployProject.sh
+++ scripts/DeployProject.sh
@@ -7,22 +7,22 @@ sfdx force:apex:test:run -c -u MyComponents -r human
 
 #No Longer Needed
 #echo "*** Creating Managed Package ..."
-##sfdx force:package:create --name "Demo Reset Tools" --packagetype Managed --path "force-app" --targetdevhubusername MyComponents
+#sfdx force:package:create --name "Demo Reset Tools" --packagetype Managed --path "force-app"
+#sfdx force:package1:version:create --packageid 033xx00000007oi --name ”Spring 17” --description ”Spring 17 Release” --version 3.2 --managedreleased
 
 #echo "*** Creating Unlocked Package ..."
-##sfdx force:package:create --name "Demo Reset Tools Unlocked" --packagetype Unlocked --path "force-app" --targetdevhubusername MyComponents
-
+#sfdx force:package:create --name "Demo Reset Tools Unlocked" --packagetype Unlocked --path "force-app"
 
 echo "*** Creating Package Version..."
-sfdx force:package:version:create --package "Demo Reset Tools" --skipvalidation -k test1234 --wait 10 
+sfdx force:package:version:create --package "Demo Reset Tools" -k test1234 --wait 10 --codecoverage
 
 
 echo "*** Creating Unlocked Package Version..."
-sfdx force:package:version:create --package "Demo Reset Tools Unlocked" --skipvalidation -k test1234 --wait 10 
+sfdx force:package:version:create --package "Demo Reset Tools Unlocked" --skipvalidation -k test1234 --wait 10 --codecoverage 
 
 
 echo "*** Promoting Package ..."
-sfdx force:package:version:promote -p "Demo Reset Tools@0.1.0-6"
+sfdx force:package:version:promote -p "Demo Reset Tools@0.1.0-10"
 
 
 echo "*** Pushing Package to Package Manager Org ..."
