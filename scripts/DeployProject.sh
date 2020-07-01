4f57c15c1b7f51e19c05a95c25a6cbe4396a05cf
David Larrimore <davidlarrimore@gmail.com>
2020-07-01 10:26:01 -0400

    Updated Version

--- scripts/DeployProject.sh
+++ scripts/DeployProject.sh
@@ -1,10 +1,3 @@
-
-echo "*** Deploying Source to Org ..."
-sfdx force:source:deploy --targetusername MyComponents --sourcepath force-app
-
-echo "*** Run Test Cases W\Coverage ..."
-sfdx force:apex:test:run -c -u MyComponents -r human
-
 #No Longer Needed
 #echo "*** Creating Managed Package ..."
 #sfdx force:package:create --name "Demo Reset Tools" --packagetype Managed --path "force-app"
@@ -13,17 +6,17 @@ sfdx force:apex:test:run -c -u MyComponents -r human
 #echo "*** Creating Unlocked Package ..."
 #sfdx force:package:create --name "Demo Reset Tools Unlocked" --packagetype Unlocked --path "force-app"
 
-echo "*** Creating Package Version..."
-sfdx force:package:version:create --package "Demo Reset Tools" -k test1234 --wait 10 --codecoverage
-
+#echo "*** Creating Package Version..."
+#sfdx force:package:version:create --package "Demo Reset Tools" -x --wait 10 --codecoverage
 
 echo "*** Creating Unlocked Package Version..."
-sfdx force:package:version:create --package "Demo Reset Tools Unlocked" --skipvalidation -k test1234 --wait 10 --codecoverage 
+sfdx force:package:version:create --package "Demo Reset Tools Unlocked" -x --wait 10 --codecoverage 
 
+#echo "*** Promoting Latest Managed Package ..."
+#sfdx force:package:version:promote -p $(sfdx force:package:version:list -p 'Demo Reset Tools' -o CreatedDate --concise | tail -1 | awk '{print $3}')
 
-echo "*** Promoting Package ..."
-sfdx force:package:version:promote -p "Demo Reset Tools@0.1.0-10"
+echo "*** Promoting Latest Unlocked Package ..."
+sfdx force:package:version:promote -p $(sfdx force:package:version:list -p 'Demo Reset Tools Unlocked' -o CreatedDate --concise | tail -1 | awk '{print $3}')
 
-
-echo "*** Pushing Package to Package Manager Org ..."
+#echo "*** Pushing Package to Package Manager Org ..."
 #sfdx force:package:install --package "Demo Reset Tools@0.1.0-6" --targetusername PackageManager --installationkey test1234
